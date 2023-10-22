29f066e26ec3cdcca5f3071b8562afe590fe6b14
Hsiang-Jen Li <hsiangjenli@gmail.com>
2023-10-23 00:39:50 +0800

    feat: auto download

--- Makefile
+++ Makefile
@@ -8,18 +8,18 @@ all: download unzip unzip2bytescode
 
 # Download the motif_reports.csv file from the MOTIF repository =============================================================
 download:
-	mkdir $(ZIP_FOLDER)
+	mkdir -p $(ZIP_FOLDER)
 	curl -o motif_reports.csv https://raw.githubusercontent.com/boozallen/MOTIF/68525d4896f2ca33c8b0c27e8bfe9a52e8c93967/dataset/motif_reports.csv
 	python vs.py --username $(USERNAME) --password $(PASSWORD) --output_folder $(ZIP_FOLDER) --family icedid
 	python vs.py --username $(USERNAME) --password $(PASSWORD) --output_folder $(ZIP_FOLDER) --family maze
 	python vs.py --username $(USERNAME) --password $(PASSWORD) --output_folder $(ZIP_FOLDER) --family azorult
 
 unzip:
-	mkdir $(UNZIPPED_FOLDER)
+	mkdir -p $(UNZIPPED_FOLDER)
 	unzip $(ZIP_FOLDER)/*.zip -d $(UNZIPPED_FOLDER)
 
 unzip2bytescode:
-	mkdir $(BYTESCODE_FOLDER)
+	mkdir -p $(BYTESCODE_FOLDER)
 	sudo apt-get install xxd -y
 	for file in $(UNZIPPED_FOLDER)/*.; do
 		xxd -b $$file > $(BYTESCODE_FOLDER)/$$(basename $$file).txt
--- vs.py
+++ vs.py
@@ -1,3 +1,4 @@
+import time
 import argparse
 import requests
 import pandas as pd
@@ -11,7 +12,14 @@ parser.add_argument('--output_folder', help='Output folder')
 
 args = parser.parse_args()
 
-headers = {'Accept': '*/*', 'X-User-IP': '1.1.1.1'}
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+
+def download_sha256(soup: BeautifulSoup):
+    tds = soup.find_all("tr")
+
+    for i, row in enumerate(tds):
+        if row.td.text == "SHA256":
+            return tds[i].text.replace("SHA256", "")
 
 def check_file_hash_exist_in_vs(soup: BeautifulSoup):
     for c in soup.find_all("center"):
@@ -24,18 +32,25 @@ def login_vs(username, password):
     data = {'username': username, 'password': password}
     session = requests.Session()
     r = session.post(url, data=data, headers=headers)
-    
-    return session
+    return session, session.cookies.get_dict()
 
 if __name__ == '__main__':
     available_files = pd.read_csv('motif_reports.csv', encoding='ISO-8859-1')
-    session = login_vs(args.username, args.password)
+    session, cookies = login_vs(args.username, args.password)
     df = available_files[available_files["Reported family"] == args.family]
 
     for file in df["File hash"]:
         r = session.post("https://virusshare.com/search", data={'search': file}, headers=headers)
         file_exist = check_file_hash_exist_in_vs(BeautifulSoup(r.text, 'html.parser'))
+
         if file_exist:
-            d = session.get(f"https://virusshare.com/download?{file}", headers=headers)
-            with open(f"{args.output_folder}/{args.family}_{file}.zip", 'wb') as f:
-                f.write(d.content)
+            sha256 = download_sha256(BeautifulSoup(r.text, 'html.parser'))
+            session, cookies = login_vs(args.username, args.password)
+            d = session.get(f"https://virusshare.com/download?{sha256}", cookies=cookies)
+            if d.status_code == 200:
+                with open(f"{args.output_folder}/{args.family}_{file}.zip", 'wb') as f:
+                    f.write(d.content)
+            else:
+                print(f"{d.status_code} - {file}")
+        
+        time.sleep(10)
